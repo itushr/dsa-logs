421a0538679a1f4081213916631c40fc25025dd7
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-16 17:49:06 +0530

    rejected

---
+++ leetcode/83.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* node = head;
+
+        while(node && node->next) {
+            if(node->next->val != node->val){
+                node = node->next;
+                continue;
+            }
+
+            while(node && node->next && node->next->val == node->val) {
+                node->next = node->next->next;
+                node = node->next;
+            }
+        }
+
+        return head;
+    }
+};
