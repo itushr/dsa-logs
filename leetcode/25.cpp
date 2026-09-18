0a82d8ba4ea343d10a6f88aebc64e81653260138
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-18 23:54:08 +0530

    rejected

---
+++ leetcode/25.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    pair<ListNode*, ListNode*> duck(ListNode* node, int k, int kmax) {
+        if(!node) return { nullptr, nullptr };
+
+        if(k == 1) {
+            auto [newHead, nextNewHead] = duck(node->next, kmax, kmax);
+            return { node, nextNewHead };
+        }
+
+        auto [newHead, nextNewHead] = duck(node->next, k-1, kmax);
+        if(!newHead) return { nullptr, nullptr };
+        node->next->next = node;
+        if(k == kmax) {
+            return { nullptr, newHead };
+        }
+        return { newHead, nextNewHead };
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        return duck(head, k, k).second;
+    }
+};
