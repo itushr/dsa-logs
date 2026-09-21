b9ace0bd1f73cbadf7bcc07df1a855f47d4bdfde
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-21 16:45:44 +0530

    rejected

---
+++ leetcode/28.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int l1 = haystack.size();
+        int l2 = needle.size();
+
+        for(int i=0; i<=l1-l2; i++) {
+            for(int j=0; j<l2; j++) {
+                if(haystack[i+j] != needle[j]) {
+                    break;
+                }
+
+                if(j == l2-1) {
+                    return j-l2+1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
