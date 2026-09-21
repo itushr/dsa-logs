d27516e43775018e9622f986f6654022923a2304
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-21 17:30:08 +0530

    rejected

---
+++ leetcode/1021.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int count = 0;
+
+        for(char c: s) {
+            if(c == '(' && count) {
+                count++;
+            }else{
+                count--;
+            }
+
+            if(count == 1 && c == '(') continue;
+            if(count == 0 && c == ')') continue;
+
+            ans += c;
+        }
+
+        return ans;
+    }
+};
