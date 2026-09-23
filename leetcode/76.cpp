c379844b6d6b98605a2fdb00fda4ebafdbad63df
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-23 17:41:42 +0530

    rejected

---
+++ leetcode/76.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int tfreq[255] = {0};
+        int sfreq[255] = {0};
+
+        for (char c : t) {
+            tfreq[c]++;
+        }
+
+        int wl = 0;
+        int wr = 0;
+        int wmin = 0;
+        int wminl = 0;
+        int wminr = 0;
+        int ntnodes = 0;
+
+        while (wr < s.size()) {
+            sfreq[s[wr]]++;
+
+            while (wl<wr && sfreq[s[wl]] > tfreq[s[wl]]) {
+                sfreq[s[wl]]--;
+                wl++;
+            }
+
+            if(tfreq[s[wr]] > 0 && ntnodes < t.size()) {
+                ntnodes++;
+            }
+
+            if(ntnodes >= t.size()) {
+                wmin = min(wmin, wr-wl+1);
+                wminl = wl;
+                wminr = wr;
+            }
+
+            wr++;
+        }
+
+        if(ntnodes < t.size()) {
+            return "";
+        }
+
+        return s.substr(wminl, wminr-wminl+1);
+    }
+};
