d8ad4d010416a4c41bcc6285fc18ac4d343e1fb1
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-24 17:00:53 +0530

    accepted

---
+++ leetcode/70.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+
+        int a = 1;
+        int b = 2;
+
+        for(int i=0; i<n-2; i++) {
+            int tmp = a; 
+            a = b;
+            b += tmp;
+        }
+
+        return b;
+    }
+};
