b0f2a6f5b7636a6367e7668ad18251a5a358f9d4
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-25 03:39:36 +0530

    rejected

---
+++ leetcode/91.v3.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int a;
+        int b;
+
+        string x;
+
+        if(s.size() == 1){
+            if(s[0] == '0') {
+                return 0;
+            }else{
+                return 1;
+            }
+        }
+
+        if(s[0] == '0') {
+            a = 0;
+            b = 0;
+        }else{
+            a = 1;
+            x = {s[0], s[1]};
+            if(s[1] == '0') {
+                b = 1;
+            }else if(stoi(x) > 26) {
+                b = 1;
+            }else {
+                b = 2;
+            }
+        }
+
+        for(int i=2; i<s.size(); i++) {
+            int tmp = a;
+            a = b;
+            if(s[i] == '0') {
+                if(s[i-1] == '0') {
+                    b = 0;
+                }else {
+                    b = tmp;
+                }
+            }else{
+                x = {s[i-1], s[i]};
+
+                if(s[i-1] != '0' && stoi(x) <= 26) {
+                    b += tmp;
+                }
+            }
+        }
+
+        return b;
+    }
+};
